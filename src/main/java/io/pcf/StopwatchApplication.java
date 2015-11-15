6166e564997cc16f42539dae95ea4f03b6516d1b
Kyle Travis <kyle@kyletravis.com>
2015-11-15 11:14:17 -0600

    added in timestamp code

--- src/main/java/io/pcf/StopwatchApplication.java
+++ src/main/java/io/pcf/StopwatchApplication.java
@@ -1,5 +1,10 @@
 package io.pcf;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +16,22 @@ public class StopwatchApplication {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+		// get current date time with Date()
+		Date date = new Date(0);
+		System.out.println(dateFormat.format(date));
+
+		// get current date time with Calendar()
+		Calendar cal = Calendar.getInstance();
+		// System.out.println(dateFormat.format(cal.getTime()));
+
+		// return "Hello Docker World";
+		return dateFormat.format(cal.getTime());
 	}
-	
-    public static void main(String[] args) {
-        SpringApplication.run(StopwatchApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(StopwatchApplication.class, args);
+	}
 }
